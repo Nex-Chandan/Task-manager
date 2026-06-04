89396f5256384521d1cd775b11261a3d0c77a37d
ChandanPalariya <Chandan0406@nexzem.com>
2026-06-04 19:34:23 +0530

    intialize backend repo

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("server running");
+});
+
+app.listen(port, () => {
+    console.log(`server started at ${port}`);
+});
